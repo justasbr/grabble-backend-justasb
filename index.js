d46a8086d823937c2970af3c6acb14a4fa335e76
justasbr <justasbr@gmail.com>
2016-10-01 20:41:10 +0100

    Extract routing to seperate file

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var routes = require('./app/routes');
+
+mongoose.connect('mongodb://localhost/test');
+
+app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/grabletter', function (req, res) {
-    res.json({});
-});
-
-app.post('/submitword', function (req, res) {
-    res.json({});
-});
-
-app.get('/inventory', function (req, res) {
-    res.json({})
-});
-
-app.get('/leaderboard', function (req, res) {
-    res.json({})
-});
-
-app.get('/profile', function (req, res) {
-    res.json({})
-});
-
-app.get('/placemarks', function(req,res) {
-    res.json({})
-});
+app.use('/', routes);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-    console.log('Listening on ' + port);
+  console.log('Listening on ' + port);
 });
