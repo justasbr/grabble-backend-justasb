6419ef255c8d238ff3ced8f661a4ad2ccd75597e
justasbr <justasbr@gmail.com>
2016-10-01 20:54:12 +0100

    Profile endpoint added

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,20 @@
 const routes = require('express').Router();
 
+var User = require('./models/user');
+
 routes.get('/profile', function (req, res) {
-  res.json({})
+  var userId = req.query.id || null;
+  if (!userId) {
+    res.status(400).json({err: 'User ID not specified'});
+  } else {
+    User.findOne({'id': userId}, function (err, user) {
+      if (!user) {
+        user = new User({id: userId});
+        user.save();
+      }
+      res.send(user);
+    });
+  }
 });
 
 routes.get('/inventory', function (req, res) {
