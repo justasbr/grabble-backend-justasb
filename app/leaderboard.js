c6686977792c328c65411d9008293894d3f97aa6
Justas <justas@Justass-MBP.lan>
2017-01-19 17:04:09 +0200

    New endpoint for todays leaderboard

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -2,9 +2,23 @@ const routes = require('express').Router();
 
 var User = require('./models/user');
 
+//Current day leaderboard
+routes.get('/today', function (req, res) {
+  User.find({}).sort({totalPoints: -1})
+    .limit(10).select('id totalPoints -_id')
+    .exec(function (err, leaderboard) {
+      if (!err) {
+        res.json({leaderboard: leaderboard});
+      } else {
+        res.status(400).json({err: 'Could not get leaderboard'});
+      }
+    });
+});
+
+//All-time leaderboard
 routes.get('/', function (req, res) {
   User.find({}).sort({totalPoints: -1})
-    .limit(5).select('id totalPoints -_id')
+    .limit(10).select('id totalPoints -_id')
     .exec(function (err, leaderboard) {
       if (!err) {
         res.json({leaderboard: leaderboard});
