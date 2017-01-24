26622f6a6256d365a03fb0480bfaf68cbca826ed
Justas <justas@Justass-MacBook-Pro.local>
2017-01-24 12:49:37 +0000

    Random name generator test

--- test/unit/nameGenerator.js
+++ test/unit/nameGenerator.js
@@ -1,3 +1,29 @@
-/**
- * Created by justas on 1/24/17.
- */
+var should = require('chai').should();
+
+var nameGenerator = require('../../app/nameGenerator');
+
+
+describe('NameGenerator', function () {
+  it('should generate random names that are well-formatted', function (done) {
+    var randomName1 = nameGenerator.getFullUsername();
+    isWellFormattedName(randomName1).should.be.true;
+
+    var randomName2 = nameGenerator.getFullUsername();
+    isWellFormattedName(randomName2).should.be.true;
+    done();
+  })
+});
+
+function isWellFormattedName(name) {
+  return name.length >= 3 && name == capitalize(reduceWhitespace(name));
+}
+
+function capitalize(s) {
+  return s.toLowerCase().replace(/\b./g, function (w) {
+    return w.toUpperCase();
+  });
+}
+
+function reduceWhitespace(s) {
+  return s.replace(/ +(?= )/g, '').trim();
+}
