18de305783f4378db2e815371dc780a0a996f9e5
Justas <justas@Justass-MacBook-Pro.local>
2017-01-24 17:52:06 +0000

    Boilerplate for word submission integ tests

--- test/integ/submitWord.js
+++ test/integ/submitWord.js
@@ -1,5 +1,38 @@
-var should = require('chai').should();
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+
+var should = chai.should();
+var chaiHttp = require('chai-http');
+var server = require('../../index');
+
+var nameGen = require('../../app/nameGenerator');
+
+chai.use(chaiHttp);
 
 describe('Word Submitting', function () {
+  it('should allow existing user to submit word', function (done) {
 
+    done();
+  });
+
+  it('should not allow non-existing user to submit word', function (done) {
+
+    done();
+  });
+
+  it('should not allow user with missing information to submit word', function (done) {
+
+    done();
+  });
+
+  it('should update point count after a valid submission', function (done) {
+
+    done();
+  });
+
+  it('should return status code 400 if given invalid word', function (done) {
+
+    done();
+  });
 });
